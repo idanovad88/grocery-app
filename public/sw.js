69e184acd1f98e6325a7b9b42e3bf4d4988056b6
idan <idanov88@gmail.com>
2026-04-03 00:16:07 +0300

    fix pwa installability

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,22 @@
-const CACHE_NAME = 'grocery-v1';
+const CACHE_NAME = "grocery-v2";
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['/']);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
+self.addEventListener("activate", (event) => {
+event.waitUntil(
+caches.keys().then((keys) =>
+Promise.all(
+keys
+.filter((key) => key !== CACHE_NAME)
+.map((key) => caches.delete(key))
+)
+)
+);
+self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener("fetch", (event) => {
+event.respondWith(fetch(event.request));
 });
